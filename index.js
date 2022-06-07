d735e3500e9d45553a80753c9be7639205eee227
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-07 10:09:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+
+let random=Math.floor(Math.random() +1);
+let num3=34;
+let num4=15;
+let mod=num3 % num4;
+let max=Math.max(13,20,7,18,9);
